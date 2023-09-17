d8af95140ff7047e2f46517b8783872ccb101ce3
Denis Gukov <denis.guk@konomic.com>
2023-09-17 13:33:17 +0300

    init nuxt app

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	trailingComma: "none",
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+    useTabs: true,
+	bracketSpacing: true,
+	vueIndentScriptAndStyle: true,
+	printWidth: 120,
+    bracketSameLine: false,
+    arrowParens: "always",
+    endOfLine: "auto"
+};
