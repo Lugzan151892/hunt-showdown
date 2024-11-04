079de004138a053b8124f3e9c7fb4d186989956d
Denis Gukov <denis.guk@konomic.com>
2024-11-04 14:31:04 +0300

    banned_users: fixed banned users view, fixed requests and api

--- types/banned.d.ts
+++ types/banned.d.ts
@@ -1,21 +1,3 @@
-// export interface ISteamBannedUser {
-// 	avatar: string;
-// 	avatarfull: string;
-// 	avatarhash: string;
-// 	avatarmedium: string;
-// 	communityvisibilitystate: number;
-// 	lastlogoff: number;
-// 	personaname: string;
-// 	personastate: number;
-// 	personastateflags: number;
-// 	primaryclanid: string;
-// 	profilestate: number;
-// 	profileurl: string;
-// 	steamid: string;
-// 	timecreated: number;
-// 	banned: boolean;
-// }
-
 export interface ISteamBannedUser {
 	avatar: string;
 	avatarFull: string;
